c0aefb358ebd07e841b03a4fc475e8b8b839dda6
Davide <davideghz@gmail.com>
2026-04-07 12:07:27 +0200

    Blog page: remove intro text

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -40,9 +40,6 @@ export default async function BlogPage() {
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Writing on software, products, and whatever else is on my mind.
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            All my thoughts in long form.
-          </p>
         </header>
 
         <div className="mt-16 sm:mt-20 flex flex-col space-y-16">
